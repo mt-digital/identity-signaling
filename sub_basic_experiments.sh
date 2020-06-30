c2efcfe3b0d9c07a37a20d48794c131466354288
Matthew Turner <maturner01@gmail.com>
2020-06-29 22:35:14 -0700

    rename sub_invasion_experiments and set n_iter back to 500 in basic

--- sub_basic_experiments.sh
+++ sub_basic_experiments.sh
@@ -1,21 +1,21 @@
 
 printf "\nSubmitting basic disliking experiment part 1\n"
 fname=output_data/basic/disliking/part-`uuidgen`
-subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 1000 50 $fname.csv -R0.5  \
+subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 50 $fname.csv -R0.5  \
     -qfast.q -j$fname -n24 -t"04:00:00"
 
 printf "\nSubmitting basic disliking experiment part 2\n"
 fname=output_data/basic/disliking/part-`uuidgen`
-subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 1000 50 $fname.csv -R0.5  \
+subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 50 $fname.csv -R0.5  \
     -qfast.q -j$fname -n24 -t"04:00:00"
 
 
 printf "\nSubmitting basic receptivity experiment part 1\n"
 fname=output_data/basic/receptivity/part-`uuidgen`
-subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 1000 50 $fname.csv -R0.5  \
+subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 50 $fname.csv -R0.5  \
     -qfast.q -j$fname -n24 -t"04:00:00"
 
 printf "\nSubmitting basic receptivity experiment part 2\n"
 fname=output_data/basic/receptivity/part-`uuidgen`
-subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 1000 50 $fname.csv -R0.5  \
+subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 50 $fname.csv -R0.5  \
     -qfast.q -j$fname -n24 -t"04:00:00"
--- sub_invasion_experiments.sh
+++ sub_invasion_experiments.sh
@@ -1,67 +1,70 @@
 # An executable bit of documentation of how to run all the experiments used
 # for the computational modeling publication.
 
+# NOTE -- 6/29/20: Running with learning_alpha=1.0; comment out to
+# change back to default 1.25.
+
 printf "\nRunning .95, .95\n"
 fname=output_data/invasion/disliking/part-`uuidgen`
 subexp disliking 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.95 --initial_prop_churlish=0.95 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .5, .95\n"
 fname=output_data/invasion/disliking/part-`uuidgen`
 subexp disliking 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.5 --initial_prop_churlish=0.95 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .05, .95\n"
 fname=output_data/invasion/disliking/part-`uuidgen`
 subexp disliking 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.05 --initial_prop_churlish=0.95 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .95, .5\n"
 fname=output_data/invasion/disliking/part-`uuidgen`
 subexp disliking 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.95 --initial_prop_churlish=0.5 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .5, .5\n"
 fname=output_data/invasion/disliking/part-`uuidgen`
 subexp disliking 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.5 --initial_prop_churlish=0.5 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .05, .95\n"
 fname=output_data/invasion/disliking/part-`uuidgen`
 subexp disliking 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.05 --initial_prop_churlish=0.5 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .95, .05\n"
 fname=output_data/invasion/disliking/part-`uuidgen`
 subexp disliking 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.95 --initial_prop_churlish=0.05 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .5, .05\n"
 fname=output_data/invasion/disliking/part-`uuidgen`
 subexp disliking 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.5 --initial_prop_churlish=0.05 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .05, .05\n"
 fname=output_data/invasion/disliking/part-`uuidgen`
 subexp disliking 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.05 --initial_prop_churlish=0.05 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 ############################################################
@@ -72,60 +75,60 @@ printf "\nRunning .95, .95\n"
 fname=output_data/invasion/receptivity/part-`uuidgen`
 subexp receptivity 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.95 --initial_prop_churlish=0.95 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .5, .95\n"
 fname=output_data/invasion/receptivity/part-`uuidgen`
 subexp receptivity 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.5 --initial_prop_churlish=0.95 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .05, .95\n"
 fname=output_data/invasion/receptivity/part-`uuidgen`
 subexp receptivity 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.05 --initial_prop_churlish=0.95 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .95, .5\n"
 fname=output_data/invasion/receptivity/part-`uuidgen`
 subexp receptivity 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.95 --initial_prop_churlish=0.5 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .5, .5\n"
 fname=output_data/invasion/receptivity/part-`uuidgen`
 subexp receptivity 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.5 --initial_prop_churlish=0.5 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .05, .95\n"
 fname=output_data/invasion/receptivity/part-`uuidgen`
 subexp receptivity 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.05 --initial_prop_churlish=0.5 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .95, .05\n"
 fname=output_data/invasion/receptivity/part-`uuidgen`
 subexp receptivity 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.95 --initial_prop_churlish=0.05 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .5, .05\n"
 fname=output_data/invasion/receptivity/part-`uuidgen`
 subexp receptivity 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.5 --initial_prop_churlish=0.05 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
 
 
 printf "\nRunning .05, .05\n"
 fname=output_data/invasion/receptivity/part-`uuidgen`
 subexp receptivity 0.0:0.51:0.1 0.0:0.51:0.1 500 50 $fname.csv -R0.5  \
     --initial_prop_covert=0.05 --initial_prop_churlish=0.05 \
-    -qfast.q -j$fname -n24 -t"02:00:00"
+    -qfast.q -j$fname -n24 -t"04:00:00" --learning_alpha=1.0
