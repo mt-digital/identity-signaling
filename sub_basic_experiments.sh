c6cad31d5770cc6e8d35f1024351256cb0aacece
Matthew Turner <maturner01@gmail.com>
2020-07-12 15:04:31 -0700

    basic experiments split into chunks of 4 x 25 trials for disliking and receptivity

--- sub_basic_experiments.sh
+++ sub_basic_experiments.sh
@@ -1,22 +1,61 @@
 
+# printf "\nSubmitting basic receptivity experiment part 1\n"
+# fname=output_data/basic/receptivity/part-`uuidgen`
+# subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 50 $fname.csv -R0.5  \
+#     -qstd.q -j$fname -n24 -t"08:00:00"
+
+# printf "\nSubmitting basic receptivity experiment part 2\n"
+# fname=output_data/basic/receptivity/part-`uuidgen`
+# subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 50 $fname.csv -R0.5  \
+#     -qstd.q -j$fname -n24 -t"08:00:00"
+
+# printf "\nSubmitting basic disliking experiment part 1\n"
+# fname=output_data/basic/disliking/part-`uuidgen`
+# subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 50 $fname.csv -R0.5  \
+#     -qstd.q -j$fname -n24 -t"08:00:00"
+
+# printf "\nSubmitting basic disliking experiment part 2\n"
+# fname=output_data/basic/disliking/part-`uuidgen`
+# subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 50 $fname.csv -R0.5  \
+#     -qstd.q -j$fname -n24 -t"08:00:00"
+
+
 printf "\nSubmitting basic receptivity experiment part 1\n"
-fname=output_data/basic/receptivity/part-`uuidgen`
-subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 10 $fname.csv -R0.5  \
-    -qstd.q -j$fname -n24 -t"04:00:00"
+fname=output_data/"basic_beta=2.0"/receptivity/part-`uuidgen`
+subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
+    -qstd.q -j$fname -n24 -t"08:00:00" --learning_beta=2.0
 
 printf "\nSubmitting basic receptivity experiment part 2\n"
-fname=output_data/basic/receptivity/part-`uuidgen`
-subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 10 $fname.csv -R0.5  \
-    -qstd.q -j$fname -n24 -t"04:00:00"
+fname=output_data/"basic_beta=2.0"/receptivity/part-`uuidgen`
+subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
+    -qstd.q -j$fname -n24 -t"08:00:00" --learning_beta=2.0
 
 printf "\nSubmitting basic disliking experiment part 1\n"
-fname=output_data/basic/disliking/part-`uuidgen`
-subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 10 $fname.csv -R0.5  \
-    -qstd.q -j$fname -n24 -t"04:00:00"
+fname=output_data/"basic_beta=2.0"/disliking/part-`uuidgen`
+subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
+    -qstd.q -j$fname -n24 -t"08:00:00" --learning_beta=2.0
 
 printf "\nSubmitting basic disliking experiment part 2\n"
-fname=output_data/basic/disliking/part-`uuidgen`
-subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 10 $fname.csv -R0.5  \
-    -qstd.q -j$fname -n24 -t"04:00:00"
+fname=output_data/"basic_beta=2.0"/disliking/part-`uuidgen`
+subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
+    -qstd.q -j$fname -n24 -t"08:00:00" --learning_beta=2.0
 
+printf "\nSubmitting basic receptivity experiment part 1\n"
+fname=output_data/"basic_beta=2.0"/receptivity/part-`uuidgen`
+subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
+    -qstd.q -j$fname -n24 -t"08:00:00" --learning_beta=2.0
 
+printf "\nSubmitting basic receptivity experiment part 2\n"
+fname=output_data/"basic_beta=2.0"/receptivity/part-`uuidgen`
+subexp receptivity 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
+    -qstd.q -j$fname -n24 -t"08:00:00" --learning_beta=2.0
+
+printf "\nSubmitting basic disliking experiment part 1\n"
+fname=output_data/"basic_beta=2.0"/disliking/part-`uuidgen`
+subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
+    -qstd.q -j$fname -n24 -t"08:00:00" --learning_beta=2.0
+
+printf "\nSubmitting basic disliking experiment part 2\n"
+fname=output_data/"basic_beta=2.0"/disliking/part-`uuidgen`
+subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
+    -qstd.q -j$fname -n24 -t"08:00:00" --learning_beta=2.0
