772fcc1dc80fcdba597700fc5ad06b217a07fe29
Matthew Turner <maturner01@gmail.com>
2020-08-03 18:26:26 -0700

    modified minority experiment for debug and future full 100-interaction runs

--- cl_scripts/sub_minority_experiments.sh
+++ cl_scripts/sub_minority_experiments.sh
@@ -6,7 +6,7 @@
 #     for minority_trait_frac in 0.10 0.20; do 
 
 #         fname=data/minority/$minority_trait_frac/part-`uuidgen`
-#         subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 100 $fname.csv -R0.5  \
+#         subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 500 100 $fname.csv -R1.0  \
 #             -qfast.q -j$fname -n24 -t"04:00:00" -m$minority_trait_frac \
 #             -K$K -S$similarity_threshold 
 #     done
@@ -18,7 +18,7 @@
 #     for minority_trait_frac in 0.10 0.20; do 
 
 #         fname=data/minority/$minority_trait_frac/part-`uuidgen`
-#         subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 100 $fname.csv -R0.5  \
+#         subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 500 100 $fname.csv -R1.0  \
 #             -qfast.q -j$fname -n24 -t"04:00:00" -m$minority_trait_frac \
 #             -K$K -S$similarity_threshold 
 #     done
@@ -26,54 +26,58 @@
 
 
 K=3
-for similarity_threshold in 0.3 0.5 0.8; do 
-    for minority_trait_frac in 0.10 0.20; do 
+# for similarity_threshold in 0.3 0.5 0.8; do 
+for similarity_threshold in 0.5; do 
+    for minority_trait_frac in 0.10; do 
+    # for minority_trait_frac in 0.10 0.20; do 
 
-        fname=data/"minority"/$minority_trait_frac/part-`uuidgen`
-        subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
-            -qstd.q -j$fname -n24 -t"06:00:00" -m$minority_trait_frac \
+        fname=data/"minority-debug"/$minority_trait_frac/part-`uuidgen`
+        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 25 $fname.csv -R1.0  \
+            -qstd.q -j$fname -n24 -t"05:00:00" -m$minority_trait_frac \
             -K$K -S$similarity_threshold
 
-        fname=data/"minority"/$minority_trait_frac/part-`uuidgen`
-        subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
-            -qstd.q -j$fname -n24 -t"06:00:00" -m$minority_trait_frac \
+        fname=data/"minority-debug"/$minority_trait_frac/part-`uuidgen`
+        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 25 $fname.csv -R1.0  \
+            -qstd.q -j$fname -n24 -t"05:00:00" -m$minority_trait_frac \
             -K$K -S$similarity_threshold
 
-        fname=data/"minority"/$minority_trait_frac/part-`uuidgen`
-        subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
-            -qstd.q -j$fname -n24 -t"06:00:00" -m$minority_trait_frac \
+        fname=data/"minority-debug"/$minority_trait_frac/part-`uuidgen`
+        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 25 $fname.csv -R1.0  \
+            -qstd.q -j$fname -n24 -t"05:00:00" -m$minority_trait_frac \
             -K$K -S$similarity_threshold
 
-        fname=data/"minority"/$minority_trait_frac/part-`uuidgen`
-        subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
-            -qstd.q -j$fname -n24 -t"06:00:00" -m$minority_trait_frac \
+        fname=data/"minority-debug"/$minority_trait_frac/part-`uuidgen`
+        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 25 $fname.csv -R1.0  \
+            -qstd.q -j$fname -n24 -t"05:00:00" -m$minority_trait_frac \
             -K$K -S$similarity_threshold
     done
 done
 
 
 K=9
-for similarity_threshold in 0.3 0.5 0.8; do 
-    for minority_trait_frac in 0.10 0.20; do 
+# for similarity_threshold in 0.3 0.5 0.8; do 
+for similarity_threshold in 0.8; do 
+    for minority_trait_frac in 0.20; do 
+    # for minority_trait_frac in 0.10 0.20; do 
 
-        fname=data/"minority"/$minority_trait_frac/part-`uuidgen`
-        subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
-            -qstd.q -j$fname -n24 -t"06:00:00" -m$minority_trait_frac \
+        fname=data/"minority-debug"/$minority_trait_frac/part-`uuidgen`
+        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 25 $fname.csv -R1.0  \
+            -qstd.q -j$fname -n24 -t"05:00:00" -m$minority_trait_frac \
             -K$K -S$similarity_threshold
 
-        fname=data/"minority"/$minority_trait_frac/part-`uuidgen`
-        subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
-            -qstd.q -j$fname -n24 -t"06:00:00" -m$minority_trait_frac \
+        fname=data/"minority-debug"/$minority_trait_frac/part-`uuidgen`
+        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 25 $fname.csv -R1.0  \
+            -qstd.q -j$fname -n24 -t"05:00:00" -m$minority_trait_frac \
             -K$K -S$similarity_threshold
 
-        fname=data/"minority"/$minority_trait_frac/part-`uuidgen`
-        subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
-            -qstd.q -j$fname -n24 -t"06:00:00" -m$minority_trait_frac \
+        fname=data/"minority-debug"/$minority_trait_frac/part-`uuidgen`
+        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 25 $fname.csv -R1.0  \
+            -qstd.q -j$fname -n24 -t"05:00:00" -m$minority_trait_frac \
             -K$K -S$similarity_threshold
 
-        fname=data/"minority"/$minority_trait_frac/part-`uuidgen`
-        subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 25 $fname.csv -R0.5  \
-            -qstd.q -j$fname -n24 -t"06:00:00" -m$minority_trait_frac \
+        fname=data/"minority-debug"/$minority_trait_frac/part-`uuidgen`
+        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 25 $fname.csv -R1.0  \
+            -qstd.q -j$fname -n24 -t"05:00:00" -m$minority_trait_frac \
             -K$K -S$similarity_threshold
     done
 done
@@ -84,7 +88,7 @@ done
 #     for minority_trait_frac in 0.10 0.20; do 
 
 #         fname=data/minority/$minority_trait_frac/part-`uuidgen`
-#         subexp disliking 0.0:0.51:0.05 0.0:0.51:0.05 500 20 $fname.csv -R0.5  \
+#         subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 500 20 $fname.csv -R1.0  \
 #             -qfast.q -j$fname -n24 -t"04:00:00" -m$minority_trait_frac \
 #             -K$K -S$similarity_threshold 
 #     done
