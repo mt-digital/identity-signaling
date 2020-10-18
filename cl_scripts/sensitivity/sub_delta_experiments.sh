9eae81b89636fd7d17cc8b29de535d6991eb1a83
Matthew Turner <maturner01@gmail.com>
2020-10-18 15:48:11 -0700

    debugged runner for delta sensitivity experiments

--- cl_scripts/sensitivity/sub_delta_experiments.sh
+++ cl_scripts/sensitivity/sub_delta_experiments.sh
@@ -1,6 +1,11 @@
-
-
-## DISLIKING ##
+##
+# This script co-varies the two disliking penalties, d and delta. d 
+# is the penalty when only one individual dislikes the other, and
+# delta is the penalty when both agents dislike one another.
+#
+# Author: Matthew A. Turner
+# Date: 10-18-2020
+#
 
 # First part: set delta to be three different values and run basic
 # experiment.
@@ -8,12 +13,12 @@ for delta in 0.05 0.45 0.65 0.85; do
 
     # Submit 100 trials for each setting by splitting into four sets of 
     # 25 trials.
-    for _ in {1..5}; do
-        printf "\nSubmitting basic disliking experiment part 1\n"
+    for ii in {1..5}; do
+        printf "\nSubmitting delta static experiment for delta=$delta, part $ii\n"
         fname=data/delta_static/delta\=$delta/part-`uuidgen`
         subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R1.0  \
             --two_dislike_penalty=$delta \
-            -qfast.q -j$fname -n24 -t"04:00:00"
+            -qfast.q -j$fname -n24 -t"04:00:00" 
     done
 
 done
@@ -25,10 +30,10 @@ for delta in `seq 0.0 0.1 1.0`; do
 
     # Submit 100 trials for each setting by splitting into four sets of 
     # 25 trials.
-    for _ in {1..5}; do
-        printf "\nSubmitting basic disliking experiment part 1\n"
+    for ii in {1..5}; do
+        printf "\nSubmitting delta varies experiment for delta=$delta, part $ii\n"
         fname=data/delta_varies/delta\=$delta/part-`uuidgen`
-        subexp disliking 0.0:1.01:0.10 0.05:0.86:0.2 100 10 $fname.csv -R1.0  \
+        subexp disliking 0.05:0.86:0.2 0.0:1.01:0.10 100 10 $fname.csv -R1.0  \
             --two_dislike_penalty=$delta \
             -qfast.q -j$fname -n24 -t"04:00:00"
     done
