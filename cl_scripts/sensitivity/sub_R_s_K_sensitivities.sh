2a7ae965cce0f23e7532f3baa2409e3b3182411c
Matthew Turner <maturner01@gmail.com>
2020-11-18 22:32:31 -0800

    final version of sensitivity analyses used in paper

--- cl_scripts/sensitivity/sub_R_s_K_sensitivities.sh
+++ cl_scripts/sensitivity/sub_R_s_K_sensitivities.sh
@@ -17,16 +17,6 @@ for R in 0.33 0.66; do
     subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R$R  \
         -qfast-ib.q -j$fname -n24 -t"04:00:00"
 
-    printf "\nSubmitting overt signaling efficiency disliking experiment part 1\n"
-    fname=data/"basic-overt_efficiency=$R"/disliking/part-`uuidgen`
-    subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R$R  \
-        -qfast-ib.q -j$fname -n24 -t"04:00:00"
-
-    printf "\nSubmitting overt signaling efficiency disliking experiment part 2\n"
-    fname=data/"basic-overt_efficiency=$R"/disliking/part-`uuidgen`
-    subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R$R  \
-        -qfast-ib.q -j$fname -n24 -t"04:00:00"
-
     ## COVERT RECEPTIVITY ##
     printf "\nSubmitting overt signaling efficiency receptivity experiment part 1\n"
     fname=data/"basic-overt_efficiency=$R"/receptivity/part-`uuidgen`
@@ -37,69 +27,58 @@ for R in 0.33 0.66; do
     fname=data/"basic-overt_efficiency=$R"/receptivity/part-`uuidgen`
     subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R$R  \
         -qfast-ib.q -j$fname -n24 -t"04:00:00"
-
-    printf "\nSubmitting overt signaling efficiency receptivity experiment part 3\n"
-    fname=data/"basic-overt_efficiency=$R"/receptivity/part-`uuidgen`
-    subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R$R  \
-        -qfast-ib.q -j$fname -n24 -t"04:00:00"
-
-    printf "\nSubmitting overt signaling efficiency receptivity experiment part 4\n"
-    fname=data/"basic-overt_efficiency=$R"/receptivity/part-`uuidgen`
-    subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R$R  \
-        -qfast-ib.q -j$fname -n24 -t"04:00:00"
-
 done
 
 
 # Ugly to just copy and paste, but it's sensitivity analysis time, 
 # time to git r dun.
-# for K in 3 15 21; do
-#     printf "\nSubmitting K sensitivity receptivity experiment part 1\n"
-#     fname=data/"basic-K=$K"/receptivity/part-`uuidgen`
-#     subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -K$K  \
-#         -qfast-ib.q -j$fname -n24 -t"04:00:00"
+for K in 3 21; do
+    printf "\nSubmitting K sensitivity receptivity experiment part 1\n"
+    fname=data/"basic-K=$K"/receptivity/part-`uuidgen`
+    subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -K$K  \
+        -qfast-ib.q -j$fname -n24 -t"04:00:00"
 
-#     printf "\nSubmitting K sensitivity receptivity experiment part 2\n"
-#     fname=data/"basic-K=$K"/receptivity/part-`uuidgen`
-#     subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -K$K  \
-#         -qfast-ib.q -j$fname -n24 -t"04:00:00"
+    printf "\nSubmitting K sensitivity receptivity experiment part 2\n"
+    fname=data/"basic-K=$K"/receptivity/part-`uuidgen`
+    subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -K$K  \
+        -qfast-ib.q -j$fname -n24 -t"04:00:00"
 
-#     ## DISLIKING ##
+    ## DISLIKING ##
 
-#     printf "\nSubmitting K sensitivity disliking experiment part 1\n"
-#     fname=data/"basic-K=$K"/disliking/part-`uuidgen`
-#     subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -K$K  \
-#         -qfast-ib.q -j$fname -n24 -t"04:00:00"
+    printf "\nSubmitting K sensitivity disliking experiment part 1\n"
+    fname=data/"basic-K=$K"/disliking/part-`uuidgen`
+    subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -K$K  \
+        -qfast-ib.q -j$fname -n24 -t"04:00:00"
 
-#     printf "\nSubmitting K sensitivity disliking experiment part 2\n"
-#     fname=data/"basic-K=$K"/disliking/part-`uuidgen`
-#     subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -K$K  \
-#         -qfast-ib.q -j$fname -n24 -t"04:00:00"
-# done
+    printf "\nSubmitting K sensitivity disliking experiment part 2\n"
+    fname=data/"basic-K=$K"/disliking/part-`uuidgen`
+    subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -K$K  \
+        -qfast-ib.q -j$fname -n24 -t"04:00:00"
+done
 
 
 # Ugly to just copy and paste, but it's sensitivity analysis time, 
 # time to git r dun.
-# for similarity_benefit in 0.1 0.5 1.0; do
-#     printf "\nSubmitting similarity benefit sensitivity receptivity experiment part 1\n"
-#     fname=data/"basic-s=$similarity_benefit"/receptivity/part-`uuidgen`
-#     subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv  \
-#         -qfast-ib.q -j$fname -n24 -t"04:00:00" --similarity_benefit=$similarity_benefit
+for similarity_benefit in 0.1 1.0; do
+    printf "\nSubmitting similarity benefit sensitivity receptivity experiment part 1\n"
+    fname=data/"basic-s=$similarity_benefit"/receptivity/part-`uuidgen`
+    subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv  \
+        -qfast-ib.q -j$fname -n24 -t"04:00:00" --similarity_benefit=$similarity_benefit
 
-#     printf "\nSubmitting similarity benefit sensitivity receptivity experiment part 2\n"
-#     fname=data/"basic-s=$similarity_benefit"/receptivity/part-`uuidgen`
-#     subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv  \
-#         -qfast-ib.q -j$fname -n24 -t"04:00:00" --similarity_benefit=$similarity_benefit
+    printf "\nSubmitting similarity benefit sensitivity receptivity experiment part 2\n"
+    fname=data/"basic-s=$similarity_benefit"/receptivity/part-`uuidgen`
+    subexp receptivity 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv  \
+        -qfast-ib.q -j$fname -n24 -t"04:00:00" --similarity_benefit=$similarity_benefit
 
-#     ## DISLIKING ##
+    ## DISLIKING ##
 
-#     printf "\nSubmitting similarity benefit sensitivity disliking experiment part 1\n"
-#     fname=data/"basic-s=$similarity_benefit"/disliking/part-`uuidgen`
-#     subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv  \
-#         -qfast-ib.q -j$fname -n24 -t"04:00:00" --similarity_benefit=$similarity_benefit
+    printf "\nSubmitting similarity benefit sensitivity disliking experiment part 1\n"
+    fname=data/"basic-s=$similarity_benefit"/disliking/part-`uuidgen`
+    subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv  \
+        -qfast-ib.q -j$fname -n24 -t"04:00:00" --similarity_benefit=$similarity_benefit
 
-#     printf "\nSubmitting similarity benefit sensitivity disliking experiment part 2\n"
-#     fname=data/"basic-s=$similarity_benefit"/disliking/part-`uuidgen`
-#     subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv  \
-#         -qfast-ib.q -j$fname -n24 -t"04:00:00" --similarity_benefit=$similarity_benefit
-# done
+    printf "\nSubmitting similarity benefit sensitivity disliking experiment part 2\n"
+    fname=data/"basic-s=$similarity_benefit"/disliking/part-`uuidgen`
+    subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv  \
+        -qfast-ib.q -j$fname -n24 -t"04:00:00" --similarity_benefit=$similarity_benefit
+done
--- cl_scripts/sensitivity/sub_delta_experiments.sh
+++ cl_scripts/sensitivity/sub_delta_experiments.sh
@@ -9,7 +9,41 @@
 
 # First part: set delta to be three different values and run basic
 # experiment.
-# for delta in 0.05 0.25 0.45 0.65 0.85; do
+for delta in 0.05 0.25 0.65 0.85; do
+
+    # Submit 50 trials for each setting by splitting into five sets of 
+    # 10 trials.
+    for ii in {1..5}; do
+        printf "\nSubmitting delta static experiment for delta=$delta, part $ii\n"
+        fname=data/delta_static/delta\=$delta/part-`uuidgen`
+        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R1.0  \
+            --two_dislike_penalty=$delta \
+            -qfast-ib.q -j$fname -n24 -t"04:00:00" 
+    done
+
+done
+
+
+# Next: vary delta over range with three different one-agent disliking
+# penalty.
+for delta in `seq 0.0 0.1 1.0`; do
+# for delta in 0.2; do
+
+    # Submit 50 trials for each setting by splitting into five sets of 
+    # 10 trials.
+    # for ii in {1..5}; do
+    for ii in {1..5}; do
+        printf "\nSubmitting delta varies experiment for delta=$delta, part $ii\n"
+        fname=data/delta_varies/delta\=$delta/part-`uuidgen`
+        subexp disliking 0.05:0.86:0.2 0.0:0.51:0.05 100 10 $fname.csv -R1.0  \
+            --two_dislike_penalty=$delta \
+            -qfast-ib.q -j$fname -n24 -t"04:00:00"
+    done
+
+done
+
+
+### XXX FOR FILLING IN SLOW 0.25 ONES, WHY SLOW??? XXX ###
 # for delta in 0.25; do
 
 #     # Submit 50 trials for each setting by splitting into five sets of 
@@ -19,42 +53,7 @@
 #         fname=data/delta_static/delta\=$delta/part-`uuidgen`
 #         subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R1.0  \
 #             --two_dislike_penalty=$delta \
-#             -qfast.q -j$fname -n24 -t"04:00:00" 
+#             -qstd.q -j$fname -n24 -t"06:00:00" 
 #     done
 
 # done
-
-
-# Next: vary delta over range with three different one-agent disliking
-# penalty.
-# for delta in `seq 0.0 0.1 1.0`; do
-# for delta in 0.2; do
-
-#     # Submit 100 trials for each setting by splitting into four sets of 
-#     # 25 trials.
-#     # for ii in {1..5}; do
-#     for ii in {1..3}; do
-#         printf "\nSubmitting delta varies experiment for delta=$delta, part $ii\n"
-#         fname=data/delta_varies/delta\=$delta/part-`uuidgen`
-#         subexp disliking 0.05:0.86:0.2 0.0:0.51:0.05 100 10 $fname.csv -R1.0  \
-#             --two_dislike_penalty=$delta \
-#             -qfast.q -j$fname -n24 -t"04:00:00"
-#     done
-
-# done
-
-
-### XXX FOR FILLING IN SLOW 0.25 ONES, WHY SLOW??? XXX ###
-for delta in 0.25; do
-
-    # Submit 50 trials for each setting by splitting into five sets of 
-    # 10 trials.
-    for ii in {1..5}; do
-        printf "\nSubmitting delta static experiment for delta=$delta, part $ii\n"
-        fname=data/delta_static/delta\=$delta/part-`uuidgen`
-        subexp disliking 0.0:1.01:0.10 0.0:0.51:0.05 100 10 $fname.csv -R1.0  \
-            --two_dislike_penalty=$delta \
-            -qstd.q -j$fname -n24 -t"06:00:00" 
-    done
-
-done
